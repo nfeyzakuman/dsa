ab44dabceb8c26038304617581972e7b1bf5abe7
nfeyzakuman <nfeyzakuman7@hotmail.com>
2024-04-02 10:55:57 +0300

    singleLL

---
+++ singleLL.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// implementation of linked list
+
+typedef struct node
+{
+  int data;
+  struct node *next;
+}node;
+
+node * createSingleLinkedList();
+
+void displayList(node *);
+
+void main()
+{
+  node* HEAD=0;
+  HEAD=createSingleLinkedList();
+  displayList(HEAD);
+}
+
+node * createSingleLinkedList()
+{
+  node *head, *newNode, *temp;
+  head=0;
+  int c, choice = 1;
+  while (choice == 1)
+  {
+    newNode = (node *)malloc(sizeof(node));
+    printf("enter the value of node:  ");
+    scanf("%d", &newNode->data);
+    newNode->next = 0;
+    if (head == 0)
+      head = temp = newNode;
+    else
+    {
+      temp->next = newNode;
+      temp = newNode;
+    }
+
+    printf("enter 1 for continue, 0 for end (1,0):  ");
+    scanf("%d", &c);
+    if (c == 1 || c == 0)
+      choice = c;
+    else
+    {
+      choice = 1;
+    }
+  }
+  return head;
+}
+
+void displayList(node *head)
+{
+  int count = 0;
+  node *temp = head;
+  while (temp->next != 0)
+  {
+    printf("%d  ->  ", temp->data);
+    temp = temp->next;
+    count++;
+  }
+  printf("%d", temp->data);
+  count++;
+  printf("\nThere are %d values in our list", count);
+}
